770cd264b3584739f87ed8821693840f4f92332d
baronvonpurr <robinhood.svensson@hotmail.com>
2023-11-28 14:28:59 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.2C-A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många tal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            int[] nummer = new int[antal];
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.Write($"Skriv in tal nr {i + 1}: ");
+                nummer[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Detta är talen som du har skrivit");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine($"Tal nr {i + 1}: {nummer[i]}");
+            }
+        }
+    }
+}
